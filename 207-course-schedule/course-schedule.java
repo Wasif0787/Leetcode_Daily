7f031be3dd3f02fc05e4708dd548f7ec69b7be44
Wasif Hussain <95441107+Wasif0787@users.noreply.github.com>
2024-12-18 21:22:08 +0530

    Time: 30 ms (7.17%) | Memory: 44.6 MB (94.81%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,38 @@
+class Solution {
+     public void countIndegree(int[][] graph, int[] indegree) {
+        for (int i = 0; i < graph.length; i++) {
+            indegree[graph[i][0]]++;
+        }
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indeg = new int[numCourses];
+        countIndegree(prerequisites, indeg);
+        Queue<Integer> q = new LinkedList<>();
+        int[] ans = new int[numCourses];
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+        int idx = 0;
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            ans[idx] = curr;
+            idx++;
+            for (int i = 0; i < prerequisites.length; i++) {
+                if (prerequisites[i][1] == curr) {
+                    int neigh = prerequisites[i][0];
+                    indeg[neigh]--;
+                    if (indeg[neigh] == 0) {
+                        q.add(neigh);
+                    }
+                }
+            }
+        }
+        if (idx < numCourses) {
+            return false;
+        }
+        return true;
+    }
+}
